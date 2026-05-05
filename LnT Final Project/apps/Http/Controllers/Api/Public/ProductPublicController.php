e54bcdde9f0294a9a81746df2912035fa37845ee
kzxnl <7brendt@gmail.com>
2026-05-05 16:02:41 +0700

    Update ProductPublicController.php

--- LnT Final Project/apps/Http/Controllers/Api/Public/ProductPublicController.php
+++ LnT Final Project/apps/Http/Controllers/Api/Public/ProductPublicController.php
@@ -1,1 +1,32 @@
+<?php
 
+namespace App\Http\Controllers\Api\Public;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductPublicController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Product::with('category');
+
+        if ($request->has('category_id')) {
+            $query->where('category_id', $request->category_id);
+        }
+
+        if ($request->has('search')) {
+            $query->where('name', 'like', '%' . $request->search . '%');
+        }
+
+        // Pagination untuk Public API
+        return response()->json($query->paginate(10));
+    }
+
+    public function show($id)
+    {
+        $product = Product::with('category')->findOrFail($id);
+        return response()->json($product);
+    }
+}
